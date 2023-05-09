9a5e18716710bb255c5099d148872d13f07ba1a2
J-Jordson <joasjordson19@gmail.com>
2023-05-09 10:10:03 -0300

    Atualizando

---
+++ envSistEst/SistEst/usuarios/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Usuarios(models.Model):
+    idUsuario = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=300, null=False)
+    cpf =  models.CharField(max_length=11, null=False)
+    num_CNH =  models.CharField(max_length=15, null=True)
+    validade_CNH = models.DateField(null=True, 
+                                    help_text="Formato <em>YYYY-MM-DD</em>")
+    telefone =  models.CharField(max_length=15, null=False)
+    email =  models.CharField(max_length=300, null=True)
